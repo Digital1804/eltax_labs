a7cf1d289c721057f5d3863272f90a2ecb654715
Digital1804 <yar.oduxowi4@gmail.com>
2024-08-08 22:31:58 +0700

    0W0

--- part2/8lab/2task/client.c
+++ part2/8lab/2task/client.c
@@ -13,11 +13,17 @@
 
 #include "info.h"
 
+typedef struct chat_line{
+    char *text;
+    char *name;
+} chat_line;
+
 WINDOW *left_win, *right_win, *down_win;
-char name[MAX_NAME_LEN + 1];
 bool running = TRUE;
 mqd_t service_queue, client_queue;
-sem_t sem;
+// Создаем уникальное имя очереди для клиента
+pthread_mutex_t m1 = PTHREAD_MUTEX_INITIALIZER;
+chat_line **chat = NULL;
 
 void print_down_win(char text[]) {
     if (down_win == NULL) {
@@ -56,19 +62,25 @@ void print_right_win(char text[]) {
     wrefresh(right_win);
 }
 
-void print_left_win(char text[]) {
+void print_left_win(int type, int chat_line_count) {
     if (left_win == NULL) {
         return;
     }
     wclear(left_win);
     box(left_win, 0, 0);
-    wmove(left_win, 1, 1);
-    wprintw(left_win, "%s", text);
+    sleep(1);
+    for (int i = 0; i < chat_line_count; i++) {
+        wmove(left_win, i + 1, 1);
+        if (type == CHAT)
+            wprintw(left_win, "%s: %s", chat[i]->name, chat[i]->text);
+        else{
+            wprintw(left_win, "%s", chat[i]->text);
+        }
+    }
     wrefresh(left_win);
 }
 
-char** create_windows(){
-    char **chat;
+int create_windows(){
     if (left_win != NULL) {
         delwin(left_win);
     }
@@ -80,10 +92,11 @@ char** create_windows(){
     }
     int height, width;
     getmaxyx(stdscr, height, width);
-    chat = (char**)calloc(height, sizeof(char*));
+    chat = (chat_line**)calloc(height*0.75, sizeof(chat_line*));
     for (int i = 0; i < height; i++) {
-        chat[i] = (char*)calloc(width*0.8, sizeof(char));
+        chat[i] = (chat_line*)calloc(MAX_SIZE, sizeof(chat_line));
     }
+    int max_chat_lines = height*0.75-2;
     left_win = newwin(height*0.75, width*0.8, 0, 0);
     right_win = newwin(height*0.75, width*0.2, 0, width*0.8);
     down_win = newwin(height-height*0.75, width, height*0.75, 0);
@@ -97,7 +110,7 @@ char** create_windows(){
     wrefresh(right_win);
     wrefresh(down_win);
     refresh();
-    return chat;
+    return max_chat_lines;
 }
 
 void init_pairs(){
@@ -107,8 +120,8 @@ void init_pairs(){
     init_pair(3, COLOR_BLUE, COLOR_BLACK);
 }
 
-void name_screen(char name[], mqd_t service_queue){
-    WINDOW * wnd;
+void start_screen(char name[], char client_queue_name[MAX_NAME_LEN + 15]){
+    WINDOW *wnd;
     message_t msg;
     msg.type = NAME;
     memset(msg.text, 0, MAX_SIZE);
@@ -120,24 +133,48 @@ void name_screen(char name[], mqd_t service_queue){
     wattron(wnd, A_BOLD);
     wprintw(wnd, "Enter your name...\n");
     wgetnstr(wnd, name, MAX_NAME_LEN);
+    if (strlen(name) == 0) {
+        start_screen(name, client_queue_name);
+        return;
+    }
     name[MAX_NAME_LEN] = 0;
     strncpy(msg.client_name, name, MAX_NAME_LEN+1);
+
+    struct mq_attr attr;
+    attr.mq_flags = 0;
+    attr.mq_maxmsg = 10;
+    attr.mq_msgsize = sizeof(message_t);
+    attr.mq_curmsgs = 0;
+
+    snprintf(client_queue_name, MAX_NAME_LEN + 15, "/client_queue_%s", name);
+    // Открываем уникальную очередь клиента
+    client_queue = mq_open(client_queue_name, O_CREAT | O_RDWR, QUEUE_PERMISSIONS, &attr);
+    if (client_queue == -1) {
+        perror("client queue mq_open");
+        exit(1);
+    }
+
+    // Отправляем имя клиента в общую очередь
+    msg.type = NAME;
+    snprintf(msg.text, MAX_SIZE, "%s", client_queue_name);
+    if (mq_send(service_queue, (char *)&msg, sizeof(msg), 0) == -1) {
+        perror("mq_send");
+        exit(1);
+    }
+
     wrefresh(wnd);
     delwin(wnd);
     curs_set(FALSE);
     endwin();
-    FILE *fp;
-    fp = fopen("log.txt", "a");
+    FILE *fp = fopen("log.txt", "a");
     fprintf(fp, "CLIENT %s send: %ld\t%s\n", name, msg.type, msg.text);
     fclose(fp);
-    if (mq_send(client_queue, (char *)&msg, sizeof(msg), 0) == -1) {
-        perror("mq_send");
-        exit(1);
-    }
+    return;
 }
 
 void *send_messages(void *arg) {
     FILE *fp;
+    char *name = (char *)arg;
     while (running){
         message_t msg;
         memset(msg.text, 0, sizeof(msg.text));
@@ -146,25 +183,25 @@ void *send_messages(void *arg) {
         msg.type = TEXT;
         strncpy(msg.client_name, name, MAX_NAME_LEN+1);
         if ((strcmp(msg.text, "exit") == 0) || (strcmp(msg.text, "e") == 0)) {
-            sem_wait(&sem);
+            pthread_mutex_lock(&m1);
             msg.type = QUIT;
-            if (mq_send(client_queue, (char *)&msg, sizeof(msg), 0) == -1) {
+            if (mq_send(service_queue, (char *)&msg, sizeof(msg), 0) == -1) {
                 perror("mq_send");
-                sem_destroy(&sem);
+                pthread_mutex_unlock(&m1);
                 exit(1);
             }
-            sem_post(&sem);
+            pthread_mutex_unlock(&m1);
             endwin();
             exit(0);
         }
         if (strncmp(msg.text, "", 1) != 0) {
-            sem_wait(&sem);
-            if (mq_send(client_queue, (char *)&msg, sizeof(msg), 0) == -1) {
+            pthread_mutex_lock(&m1);
+            if (mq_send(service_queue, (char *)&msg, sizeof(msg), 0) == -1) {
                 perror("mq_send");
-                sem_destroy(&sem);
+                pthread_mutex_unlock(&m1);
                 exit(1);
             }
-            sem_post(&sem);
+            pthread_mutex_unlock(&m1);
         }
         fp = fopen("log.txt", "a");
         fprintf(fp, "CLIENT %s send: %ld\t%s\n", name, msg.type, msg.text);
@@ -178,65 +215,116 @@ void signal_handler(int sig) {
 }
 
 int main() {
+    FILE *fp;
     initscr();
     system("clear");
-    // refresh();
     cbreak();
+    nodelay(left_win, TRUE);
+    nodelay(right_win, TRUE);
     curs_set(FALSE);
+    char name[MAX_NAME_LEN + 1];
     memset(name, 0, sizeof(name));
-    FILE *fp;
-    struct mq_attr attr;
-    attr.mq_flags = 0;
-    attr.mq_maxmsg = MAX_CLIENTS;
-    attr.mq_msgsize = sizeof(message_t);
-    attr.mq_curmsgs = 0;
     service_queue = mq_open(QUEUE_NAME, O_RDWR);
     if (service_queue == -1) {
         perror("service mq_open");
         exit(1);
     }
-    client_queue = mq_open(CLIENT_QUEUE_NAME, O_RDWR);
-    if (client_queue == -1) {
-        perror("client mq_open");
-        exit(1);
-    }
-    name_screen(name, service_queue);
+    int chat_line_count = 0;
+    char client_queue_name[MAX_NAME_LEN + 15];
+    start_screen(name, client_queue_name);
     init_pairs();
-    char **chat = create_windows(&left_win, &right_win, &down_win);
+    
+    int max_chat_lines = create_windows(&left_win, &right_win, &down_win);
     signal(SIGWINCH, signal_handler);
     wmove(down_win, 1, 1);
     keypad(down_win, TRUE);
-    sem_init(&sem, 0, 1);
     pthread_t sender_thread;
-    pthread_create(&sender_thread, NULL, send_messages, down_win);
+    pthread_create(&sender_thread, NULL, send_messages, name);
     sleep(1);
+    message_t msg;
+    msg.type = HISTORY;
+    sprintf(msg.text, "%d", max_chat_lines);
+    snprintf(msg.client_name, MAX_NAME_LEN+1, "%s", name);
+    if (mq_send(service_queue, (char *)&msg, sizeof(msg), 0) == -1) {
+        perror("mq_send");
+        exit(1);
+    }
     while (1) {
-        message_t msg;
-        if (mq_receive(service_queue, (char *)&msg, sizeof(msg), NULL) == -1) {
+        if (mq_receive(client_queue, (char *)&msg, sizeof(msg), NULL) == -1) {
             perror("mq_receive");
             exit(1);
         } 
         else {
             fp = fopen("log.txt", "a");
-            fprintf(fp, "CLIENT %s recieve: %ld\t%s\n", name, msg.type, msg.text);
+            fprintf(fp, "CLIENT %s recieve: %ld\t%s\tFROM %s\n", name, msg.type, msg.text, msg.client_name);
             fclose(fp);
             switch (msg.type) {
                 case CHAT:
-                    print_left_win(msg.text);
+                    if (chat_line_count == max_chat_lines) {
+                        free(chat[0]->text);
+                        free(chat[0]->name);
+                        free(chat[0]);         
+                        // Сдвиг элементов массива влево
+                        for (int i = 0; i < chat_line_count - 1; i++) {
+                            chat[i] = chat[i + 1];
+                        }
+                        chat_line_count--;
+
+                    }
+                    // Добавление новой строки в массив
+                    chat[chat_line_count] = (chat_line *)malloc(sizeof(chat_line));
+                    chat[chat_line_count]->text = strdup(msg.text);
+                    chat[chat_line_count]->name = strdup(msg.client_name);
+                    if (chat_line_count < max_chat_lines)  chat_line_count++;
+
+                    print_left_win(CHAT, chat_line_count);
                     break;
                 case MEMBERS:
                     print_right_win(msg.text);
                     break;
+                case HISTORY:
+                    if (chat_line_count == max_chat_lines) {
+                                             
+                        // Сдвиг элементов массива влево
+                        for (int i = 0; i < chat_line_count - 1; i++) {
+                            chat[i] = chat[i + 1];
+                        }
+                        chat_line_count--;
+
+                    }
+                    // Добавление новой строки в массив
+                    chat[chat_line_count]->text = strdup(msg.text);
+                    chat[chat_line_count]->name = strdup(msg.client_name);
+                    if (chat_line_count < max_chat_lines)  chat_line_count++;
+                    if (strncmp(msg.client_name, name, MAX_NAME_LEN + 1) == 0) {
+                        print_left_win(HISTORY, chat_line_count);
+                        break;
+                    }
                 default:
                     break;
             }
-            sleep(1);
         }
     }
     delwin(left_win);
     delwin(right_win);
     delwin(down_win);
     endwin();
-    sem_destroy(&sem);
+    for (int i = 0; i < chat_line_count; i++) {
+        free(chat[i]->text);
+        free(chat[i]->name);
+        free(chat[i]);
+    }
+    if (mq_close(service_queue) == -1) {
+        perror("service mq_close");
+        exit(1);
+    }
+    if (mq_close(client_queue) == -1) {
+        perror("service mq_close");
+        exit(1);
+    }
+    if (mq_unlink(client_queue_name) == -1) {
+        perror("service mq_unlink");
+        exit(1);
+    }
     pthread_cancel(sender_thread);
 }
--- part2/8lab/2task/server.c
+++ part2/8lab/2task/server.c
@@ -5,11 +5,17 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#include <time.h>
-#include <pthread.h>
 #include <signal.h>
+#include <pthread.h>
 #include "info.h"
+
+typedef struct {
+    char name[MAX_NAME_LEN + 1];
+    mqd_t mq;
+} client_t;
+
 int running = 1;
+
 void signal_handler(int signal) {
     if (signal == SIGINT) {
         printf("Server stopped\n");
@@ -18,22 +24,43 @@ void signal_handler(int signal) {
     }
 }
 
-void read_file_from_end(const char *filename) {
+void concatenate_strings(char *result, client_t *clients, int num_strings) {
+    result[0] = '\0';
+    for (int i = 0; i < num_strings; i++) {
+        strcat(result, clients[i].name);
+        if (i < num_strings - 1) {
+            strcat(result, " ");
+        }
+    }
+    result[strlen(result)] = '\0';
+}
+
+char **create_lines_list_from_end(const char *filename, int max_lines) {
+    int lines_count = 0;
     FILE *file = fopen(filename, "r");
     if (!file) {
         perror("Cannot open file");
-        return;
+        return NULL;
     }
 
     // Переместим указатель файла в конец
+
     fseek(file, 0, SEEK_END);
     long file_size = ftell(file);
+
+    char **lines = (char **)malloc(max_lines * sizeof(char *));
+    if (!lines) {
+        perror("Memory allocation error");
+        fclose(file);
+        return NULL;
+    }
 
     char buffer[MAX_SIZE];
     long pos = file_size;
     int i;
 
-    while (pos > 0) {
+    while (pos > 0 && lines_count < max_lines) {
+
         // Ищем конец строки
         for (i = pos - 1; i >= 0; i--) {
             fseek(file, i, SEEK_SET);
@@ -48,137 +75,220 @@ void read_file_from_end(const char *filename) {
         // Считываем строку
         fseek(file, i + 1, SEEK_SET);
         fread(buffer, 1, len, file);
+        if (strlen(buffer) < 2) continue;
         buffer[len] = '\0';
-        
-        // Выводим строку
-        printf("%s\n", buffer);
 
+        // Сохраняем строку в массиве
+        lines[lines_count] = strdup(buffer);
+        if (!lines[lines_count]) {
+            perror("Memory allocation error");
+            break;
+        }
+        lines_count++;
+
+        // Обновляем позицию
         pos = i;
     }
 
     fclose(file);
+    return lines;
 }
 
-void concatenate_strings(char *result, char strings[][MAX_NAME_LEN+1], int num_strings) {
-    // Убедимся, что результирующая строка пустая
-    result[0] = '\0';
-    for (int i = 0; i < num_strings; i++) {
-        // Проверим, что строка не пустая
-        if (strings[i] != NULL && strlen(strings[i]) > 0) {
-            strcat(result, strings[i]);
-
-            // Добавляем разделитель, если не последняя строка
-            if (i < num_strings - 1) {
-                strcat(result, " "); // Используем пробел в качестве разделителя
-            }
-        }
+void log_send(message_t msg) {
+    FILE *fp = fopen("log.txt", "a");
+    switch (msg.type) {
+        case QUIT:
+            fprintf(fp, "SERVER send QUIT\tto %s\n", msg.client_name);
+            break;
+        case CHAT:
+            fprintf(fp, "SERVER send MESSAGE\t\"%s\"\tFROM %s\n", msg.text, msg.client_name);
+            break;
+        case HISTORY:
+            fprintf(fp, "SERVER send HISTORY MESSAGE\t\"%s\"\tTO %s\n", msg.text, msg.client_name);
+            break;
+        case MEMBERS:
+            fprintf(fp, "SERVER send MEMBERS\t\"%s\"\tTO  %s\n", msg.text, msg.client_name);
+            break;
+        default:
+            break;
     }
-    result[strlen(result)] = '\0';
+    fclose(fp);
 }
 
-int main() {
-    printf("Server started\n");
-    FILE *fp, *chat;
-    fp = fopen("log.txt", "w");
+void log_rcv(message_t msg) {
+    FILE *fp = fopen("log.txt", "a");
+    switch (msg.type) {
+        case NAME:
+            fprintf(fp, "SERVER receive NEW NAME\t%s\n", msg.client_name);
+            break;
+        case QUIT:
+            fprintf(fp, "SERVER receive QUIT FROM %s\n", msg.client_name);
+            break;
+        case CHAT:
+            fprintf(fp, "SERVER receive MESSAGE\t\"%s\"\tFROM %s\n", msg.text, msg.client_name);
+            break;
+        case HISTORY:
+            fprintf(fp, "SERVER receive HISTORY REQUEST FROM %s\n", msg.client_name);
+            break;
+        case MEMBERS:
+            fprintf(fp, "SERVER receive MEMBERS REQUEST FROM %s\n", msg.client_name);
+            break;
+        default:
+            break;
+    }
     fclose(fp);
-    int client_count = 0;
-    char clients[MAX_CLIENTS][MAX_NAME_LEN+1] = {};
-    mqd_t service_queue, client_queue;
+}
+
+void create_service_queue(mqd_t *service_queue) {
     struct mq_attr attr;
 
-    // Настройка атрибутов очереди
     attr.mq_flags = 0;
     attr.mq_maxmsg = MAX_CLIENTS;
     attr.mq_msgsize = sizeof(message_t);
     attr.mq_curmsgs = 0;
-    signal(SIGINT, signal_handler);
-    // Создание очереди сообщений
-    service_queue = mq_open(QUEUE_NAME, O_CREAT | O_RDWR, QUEUE_PERMISSIONS, &attr);
-    if (service_queue == -1) {
-        perror("mq_open");
+
+    *service_queue = mq_open(QUEUE_NAME, O_CREAT | O_RDONLY, QUEUE_PERMISSIONS, &attr);
+    if (*service_queue == -1) {
+        perror("service mq_open");
         exit(1);
+    }
+}
+
+void connect_client_queue(char *client_name, mqd_t *client_queue) {
+    char client_queue_name[MAX_NAME_LEN + 15];
+    snprintf(client_queue_name, sizeof(client_queue_name), "/client_queue_%s", client_name);
+    struct mq_attr attr;
+
+    attr.mq_flags = 0;
+    attr.mq_maxmsg = 10;
+    attr.mq_msgsize = sizeof(message_t);
+    attr.mq_curmsgs = 0;
+
+    *client_queue = mq_open(client_queue_name, O_WRONLY, QUEUE_PERMISSIONS, &attr);
+    if (*client_queue == -1) {
+        perror("client queue mq_open");
+        exit(1);
+    }
+}
+
+int main() {
+    mqd_t service_queue;
+    client_t clients[MAX_CLIENTS];
+    FILE *chat;
+    int client_count = 0;
+    char client_queue_name[MAX_NAME_LEN + 15];
+    
+    create_service_queue(&service_queue);
+    signal(SIGINT, signal_handler);
+    FILE *fp = fopen("log.txt", "w");
+    fclose(fp);
+    printf("Server started\n");
+    while (running) {
+        message_t msg;
+        if (mq_receive(service_queue, (char *)&msg, sizeof(msg), NULL) == -1) {
+            perror("mq_receive");
+            continue;
+        }
+        log_rcv(msg);
+        // Обработка получаемых сообщений
+        switch (msg.type) {
+            case NAME: // Подключение клиента
+                msg.type = MEMBERS;
+                mqd_t client_queue;
+
+                connect_client_queue(msg.client_name, &client_queue);
+                chat = fopen("chat.txt", "a");
+                fprintf(chat, "%s joined the chat\n", msg.client_name);
+                fclose(chat);
+
+                if (client_count < MAX_CLIENTS) {
+                    strncpy(clients[client_count].name, msg.client_name, MAX_NAME_LEN);
+                    clients[client_count].mq = client_queue;
+                    client_count++;
+                }
+
+                concatenate_strings(msg.text, clients, client_count);
+
+                for (int i = 0; i < client_count; i++) {
+                    log_send(msg);
+                    if (mq_send(clients[i].mq, (char *)&msg, sizeof(msg), 0) == -1) {
+                        perror("mq_send");
+                    }
+                }
+                break;
+            case TEXT: // Клиент отправил сообщение
+                msg.type = CHAT;
+                chat = fopen("chat.txt", "a");
+                fprintf(chat, "%s: %s\n", msg.client_name, msg.text);
+                fclose(chat);
+
+                for (int i = 0; i < client_count; i++) {
+                    log_send(msg);
+                    if (mq_send(clients[i].mq, (char *)&msg, sizeof(msg), 0) == -1) {
+                        perror("mq_send");
+                    }
+                }
+                break;
+            case HISTORY: // Клиент запрашивает историю сообщений
+                char **lines = create_lines_list_from_end("chat.txt", atoi(msg.text));
+                char name[MAX_NAME_LEN+1];
+                strncpy(name, msg.client_name, MAX_NAME_LEN);
+                //  Отправляем строки
+                msg.type = HISTORY;
+                snprintf(msg.client_name, MAX_NAME_LEN + 1, "%s", "SERVER");
+                for (int i = 0; i < client_count; i++) {
+                    if (strcmp(clients[i].name, msg.client_name) == 0) {
+                        strncpy(msg.client_name, clients[i].name, MAX_NAME_LEN+1);
+                        client_queue = clients[i].mq;
+                        break;
+                    }
+                }
+
+                for (int i = sizeof(lines) * 2-1; i >=0 ; i--) {
+                    if (i == 0) snprintf(msg.client_name, MAX_NAME_LEN + 1, "%s", name);;
+                    strncpy(msg.text, lines[i], MAX_SIZE);
+                    log_send(msg);
+                    if (mq_send(client_queue, (char *)&msg, sizeof(msg), 0) == -1) {
+                        perror("mq_send");
+                    }
+                    free(lines[i]); // Освобождаем память, выделенную для строки
+                }
+                // snprintf(msg.client_name, MAX_NAME_LEN + 1, "%s", name);
+                // if (mq_send(client_queue, (char *)&msg, sizeof(msg), 0) == -1) {
+                //     perror("mq_send");
+                // }
+                free(lines);
+                break;
+            case QUIT: // Клиент отключается
+                msg.type = MEMBERS;
+                for (int i = 0; i < client_count; i++) {
+                    if (strcmp(clients[i].name, msg.client_name) == 0) {
+                        for (int j = i; j < client_count - 1; j++) {
+                            clients[j] = clients[j + 1];
+                        }
+                        client_count--;
+                        break;
+                    }
+                }
+                concatenate_strings(msg.text, clients, client_count);
+                for (int i = 0; i < client_count; i++) {
+                    log_send(msg);
+                    if (mq_send(clients[i].mq, (char *)&msg, sizeof(msg), 0) == -1) {
+                        perror("mq_send");
+                    }
+                }
+                break;
+        }
     }
 
-    client_queue = mq_open(CLIENT_QUEUE_NAME, O_CREAT | O_RDWR, QUEUE_PERMISSIONS, &attr); 
-    if (client_queue == -1) {
-        perror("mq_open");
-        exit(1);
-    }
-    message_t msg;
-    while (running) {
-        strncmp(msg.text, "", 1);
-        if (mq_receive(client_queue, (char *)&msg, sizeof(msg), NULL) == -1) {
-            perror("mq_receive");
-            exit(1);
-        }
-        else{
-            fp = fopen("log.txt", "a");
-            fprintf(fp, "SERVER recieve %ld\t%s\t%s\n", msg.type, msg.text, msg.client_name);
-            fclose(fp);
-            switch (msg.type) {
-                case NAME:
-                    strncpy(clients[client_count++], msg.client_name, MAX_NAME_LEN);
-                    // Заполняем именами всех клиентов
-                    concatenate_strings(msg.text, clients, client_count);
-                    msg.type = MEMBERS;
-                    for (int i = 0; i < client_count; i++) {
-                        if (mq_send(service_queue, (char *)&msg, sizeof(msg), 0) == -1) {
-                            perror("mq_send");
-                        }
-                    }
-                    fp = fopen("log.txt", "a");
-                    fprintf(fp, "SERVER send MEMBERS %s\n", msg.text);
-                    fclose(fp);
-                    break;
-                case TEXT:
-                    chat = fopen("chat.txt", "a");
-                    fprintf(chat, "%s: %s\n", msg.client_name, msg.text);
-                    fclose(chat);
-                    msg.type = CHAT;
-                    for (int i = 0; i < client_count; i++) {
-                        if (mq_send(service_queue, (char *)&msg, sizeof(msg), 0) == -1) {
-                            perror("mq_send");
-                        }
-                    }
-                    break;
-                case QUIT:
-                    for (int i = 0; i < client_count; i++) {
-                        if (strcmp(clients[i], msg.client_name) == 0) {
-                            for (int j = i; j < client_count - 1; j++) {
-                                strcpy(clients[j], clients[j + 1]);
-                            }
-                            client_count--;
-                            break;
-                        }
-                    }
-                    concatenate_strings(msg.text, clients, client_count);
-                    msg.type = MEMBERS;
-                    for (int i = 0; i < client_count; i++) {
-                        if (mq_send(service_queue, (char *)&msg, sizeof(msg), 0) == -1) {
-                            perror("mq_send");
-                        }
-                    }
-                    fprintf(fp, "SERVER send MEMBERS %s\n", msg.text);
-                    break;
-            }
-        }
-    }
     printf("Server shutting down...\n");
-    // Закрытие и удаление очереди
+
     if (mq_close(service_queue) == -1) {
         perror("service mq_close");
         exit(1);
     }
     if (mq_unlink(QUEUE_NAME) == -1) {
         perror("service mq_unlink");
-        exit(1);
-    }
-    if (mq_close(client_queue) == -1) {
-        perror("client mq_close");
-        exit(1);
-    }
-    if (mq_unlink(CLIENT_QUEUE_NAME) == -1) {
-        perror("client mq_unlink");
         exit(1);
     }
 
